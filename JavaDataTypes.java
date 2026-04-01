908ea8788602906f330f1c90d6c2c9810523f72a
Shadaan <shadaankabula@gmail.com>
2026-04-01 18:12:19 +0530

    Create JavaDataTypes.java

---
+++ JavaDataTypes.java
@@ -0,0 +1,12 @@
+public class JavaDataType{
+	public static void main(String[] args) {
+		byte age =35;
+		short salary =5000;
+		int price = 3500000;
+		long budget_amount = 500000000L;
+		System.out.println("Age="+age);
+		System.out.println("salary ="+salary);
+		System.out.println("price ="+price);
+		System.out.print("budgedt_amount="+budget_amount);
+	}
+}
