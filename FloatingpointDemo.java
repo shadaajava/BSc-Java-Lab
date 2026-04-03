48d9c07610987a47c6ac69e5f2a0cf1af3f3b2ac
Shadaan <shadaankabula@gmail.com>
2026-04-03 16:09:17 +0530

    Create FloatingpointDemo.java

---
+++ FloatingpointDemo.java
@@ -0,0 +1,8 @@
+public class FloatingpointDemo {
+	public static void main(String[] args) {
+		float interest_rate = 10.5f;
+		double pi = 3.141592654;
+		System.out.println("interest_rate="+interest_rate);
+		System.out.println("pi="+pi);
+	}
+}
