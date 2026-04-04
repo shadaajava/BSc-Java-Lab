8f73e990aae25f93c5b3c5e0760f05e327de10bd
Shadaan <shadaankabula@gmail.com>
2026-04-04 20:55:18 +0530

    Create PrintArrayElement.java

---
+++ PrintArrayElement.java
@@ -0,0 +1,9 @@
+public class PrintArrayElement{
+	public static void main(String[] args) {
+		double[] arrayList= {2.3,12.9,23.4,43.5};
+		// print all array elements
+		for(int i=0;i<4;i++){
+		    System.out.println(arrayList[i]+"");
+	}
+}
+}
