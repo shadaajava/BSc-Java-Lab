e4fd499d9c063aa944489bcd0df35f0ddd9b6f13
Shadaan <shadaankabula@gmail.com>
2026-04-06 19:26:58 +0530

    Create java-array-operations.java

---
+++ java-array-operations.java
@@ -0,0 +1,18 @@
+public class Main {
+	public static void main(String[] args) {
+		double [] arrayList = {2.3,121.9,23.4,43.5};
+		for (int i=0;i<4;i++)
+		{
+		    System.out.println(arrayList[i]+"");
+		}
+		//adding all the elements
+		double total = 0;
+		for (int i = 0; i<4;i++)
+		{
+		    total += arrayList[i];
+		    //same as total =total+arrayList[i];
+		}
+		double average = total/4;
+		System.out.println("Total is"+total);
+		System.out.println("average is"+average);
+	}
