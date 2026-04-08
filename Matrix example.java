54db77a9a29c5f24c52eff3b76af1d144e60a84c
Shadaan <shadaankabula@gmail.com>
2026-04-08 18:00:45 +0530

    Create Matrix example.java

---
+++ Matrix example.java
@@ -0,0 +1,10 @@
+public class MatrixExample{
+	public static void main(String[] args) {
+		int arrayMatrix [][] = {{1,2,3},{4,5,6},{7,8,9}};
+		for(int i=0;i<3;i++){
+		    for(int j=0;j<3;j++)
+		    System.out.print(arrayMatrix[i][j]+"");
+		    System.out.println();
+	}
+}
+	}
