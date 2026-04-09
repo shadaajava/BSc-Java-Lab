a09c64afd6aa772a5eacf4a54d9f3ebfb63fbb4e
Shadaan <shadaankabula@gmail.com>
2026-04-09 18:37:12 +0530

    Create ArithmeticOperator.java

---
+++ ArithmeticOperator.java
@@ -0,0 +1,16 @@
+public class ArithmeticOperator{
+	public static void main(String[] args) {
+		double num1=14.5, num2=2.5, result;
+		result= num1+num2;
+		System.out.println("num1+num2="+result);
+		result= num1-num2;
+		System.out.println("num1-num2="+result);
+		result=num1*num2;
+		System.out.println("num1*num2="+result);
+		result=num1/num2;
+		System.out.println("num1/num2="+result);
+		result=num1%num2;
+		System.out.println("num1/num2="+result);
+		
+	}
+}
