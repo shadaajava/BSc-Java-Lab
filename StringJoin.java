8ea590f3bd9ad4307686e9833b150520db455905
Shadaan <shadaankabula@gmail.com>
2026-04-14 17:50:07 +0530

    Create StringJoin.java

---
+++ StringJoin.java
@@ -0,0 +1,10 @@
+public class Stringjoin{
+	public static void main(String[] args) {
+		String start,middle,end,result;
+		start="java";
+		middle= "is object oriented";
+		end="programming language";
+		result=start+middle+end;
+		System.out.println(result);
+	}
+}
