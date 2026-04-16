ed95a5dfe5bfe5ea90d9b7e6480c1a7fb096bbca
Shadaan <shadaankabula@gmail.com>
2026-04-16 21:01:17 +0530

    Create Relational operator.java

---
+++ Relational operator.java
@@ -0,0 +1,12 @@
+public class RelationalOperator {
+	public static void main(String[] args) { 
+	int num1=5,num2=6;
+	if(num1>num2)
+		{
+		    System.out.println("num1 is greater than num2.");
+		}
+		else{
+		    System.out.println("num2 is greatet than num1.");
+		}
+	}
+}
