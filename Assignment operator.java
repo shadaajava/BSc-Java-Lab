f41901d74ae87467ea57c1e9104b47debb8fcf1f
Shadaan <shadaankabula@gmail.com>
2026-04-18 18:19:22 +0530

    Create Assignment operator.java

---
+++ Assignment operator.java
@@ -0,0 +1,10 @@
+public class AssignmentOperator {
+	public static void main(String[] args) {
+		int num1,num2;
+		num1=5;
+		System.out.println("num1="+num1);
+		num2=num1;
+		System.out.println("num2="+num2);
+		
+	}
+}
