ff43a025db117d25d0fb32b8dda9cd3b5f2c1997
Shadaan <shadaankabula@gmail.com>
2026-04-20 15:53:40 +0530

    Create Ternary operator.java

---
+++ Ternary operator.java
@@ -0,0 +1,9 @@
+public class TernaryOperator {
+	public static void main(String[] args) {
+		int a=5,b=6;
+		String result;
+		result = (a==b)?"equal":"not equal";
+		System.out.println(result);
+		
+	}
+}
