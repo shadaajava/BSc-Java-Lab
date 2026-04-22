dd98f3cc5e46d9cf385f494d66844c849bb62284
Shadaan <shadaankabula@gmail.com>
2026-04-22 19:24:47 +0530

    Create Ifstatement.java

---
+++ Ifstatement.java
@@ -0,0 +1,10 @@
+public class IfStatement {
+	public static void main(String[] args) {
+		int num=9;
+		if(num<10)
+		{
+		    System.out.println("number is less than 10");
+	}
+	System.out.println("this statement is always executed");
+	}
+}
