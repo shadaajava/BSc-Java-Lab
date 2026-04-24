b1e2c451df992b226bc4eb741d7dd2019565534f
Shadaan <shadaankabula@gmail.com>
2026-04-24 14:32:48 +0530

    Create Ifelsestatment.java

---
+++ Ifelsestatment.java
@@ -0,0 +1,13 @@
+public class IfelseStatment {
+	public static void main(String[] args) {
+		int num=35;
+		if(num>35){
+		System.out.println("you are pass");
+	System.out.println("congrarulations");
+}
+      else{
+    System.out.println("you are fail");
+    System.out.println("work hard");
+}
+}
+}
