12ca7d5f73c4c59ba229cad55decea57ab98c846
Shadaan <shadaankabula@gmail.com>
2026-06-11 19:16:21 +0530

    Create frame.java

---
+++ frame.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+public class frame
+{
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame("test Frame");
+        frame.setSize(300,200);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
